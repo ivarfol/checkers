5ac1e08628fb295b7c9a6e2406848f6c6d908c80
Iaroslav Varfolomeev <varfolomeeviaroslav@gmail.com>
2024-08-23 09:13:55 +0100

    follow the rule of maximum figures removed in international

--- checkers.py
+++ checkers.py
@@ -544,6 +544,15 @@ def turn_validation(board, board_turn, wh_t, l_attak, val, border, inter): #prev
             
     return(attak and (usual_sraight or usual_eats or damka_straight or damka_eats), turn)
 
+def in_list(list_1, board, colour, border):
+    flag = False
+    for val in list_1:
+        print(val)
+        print(acceptable_jump(val, board, colour, border))
+        if acceptable_jump(val, board, colour, border):
+            flag = True
+    return(flag)
+
 def colour_list(board, turn, border, inter):
     fig_list, list_eats, damka_list, temp = [], [], [], []
     if turn:
@@ -596,12 +605,15 @@ def player_turn(board, wh_tur, Ru, PvP, board_colour, tries, length, turn_number
     C_l_temp = colour_list(board, wh_tur, border, inter)
     C_l_jump = colour_list(board, wh_tur, border, False)
     turn = []
-    if Ru:
-        C_l = C_l_temp
     if wh_tur: # white
         col = ('m', 'w')
     else:
         col = ('p', 'b')
+    if Ru:
+        if in_list(C_l_jump, board, col, border):
+            C_l = C_l_jump
+        else:
+            C_l = C_l_temp
     board_turn, inv_count = validation(board, wh_tur, PvP, 0, board_colour, '', Ru, tries, length, turn_number, shift, border)
     while board_turn != 'surrender' and board_turn != 'undo' and board_turn != 'redo' and not(board[board_turn[0]][board_turn[1]] in col and board[board_turn[2]][board_turn[3]] == '0' and turn_validation(board, board_turn, wh_tur, C_l, False, border, inter)[0]):
         if PvP:
